a854d2394704dc3b9524b76965dd9f469a611d17
Gabriel Machado <g_machado1@hotmail.com>
2025-11-28 19:19:39 +0000

    chore: refactor to migrations route

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,33 +5,40 @@ import database from "infra/database";
 
 export default async function migrations(req, res) {
   if (req.method !== "GET" && req.method !== "POST") {
-    return res.status(404).send();
+    return res.status(405).json({
+      error: "Method not allowed",
+    });
   }
 
   const dbClient = await database.getNewClient();
 
-  const defaultOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  try {
+    const defaultOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationsRunner(defaultOptions);
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationsRunner(defaultOptions);
+      return res.status(200).json(pendingMigrations);
+    }
+
+    const createdMigrations = await migrationsRunner({
+      ...defaultOptions,
+      dryRun: false,
+    });
+
+    const statusCode = !createdMigrations.length ? 200 : 201;
+
+    return res.status(statusCode).json(createdMigrations);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
     await dbClient.end();
-    return res.status(200).json(pendingMigrations);
   }
-
-  const createdMigrations = await migrationsRunner({
-    ...defaultOptions,
-    dryRun: false,
-  });
-  await dbClient.end();
-
-  const statusCode = !createdMigrations.length ? 200 : 201;
-
-  return res.status(statusCode).json(createdMigrations);
 }
