31c30d279871f475685b9b8ed915fd81982132cf
Gabriel <g_machado1@hotmail.com>
2026-02-02 11:42:26 -0300

    feat: add filter of output values to migrations and status routes

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,34 +3,71 @@ import orchestrator from "tests/orchestrator";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
 });
 
 describe("POST /api/migrations", () => {
   describe("Anonymous user", () => {
-    describe("Running pending migrations", () => {
-      test("For the first time", async () => {
-        const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
-        expect(res1.status).toBe(201);
-
-        const responseBody1 = await res1.json();
-
-        expect(Array.isArray(responseBody1)).toBe(true);
-        expect(responseBody1.length).toBeGreaterThan(0);
+    test("Running pending migrations", async () => {
+      const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
       });
+      expect(res1.status).toBe(403);
 
-      test("For the second time", async () => {
-        const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
-        expect(res2.status).toBe(200);
+      const responseBody1 = await res1.json();
 
-        const responseBody2 = await res2.json();
-
-        expect(Array.isArray(responseBody2)).toBe(true);
-        expect(responseBody2.length).toEqual(0);
+      expect(responseBody1).toEqual({
+        action: 'Check if your user has the feature "create:migration"',
+        message: "You don't have permission to execute this action",
+        name: "ForbiddenError",
+        status_code: 403,
       });
+    });
+  });
+
+  describe("Default user", () => {
+    test("Running pending migrations", async () => {
+      const createdUser = await orchestrator.createUser();
+      const activatedUser = await orchestrator.activateUser(createdUser);
+      const session = await orchestrator.createSession(activatedUser.id);
+
+      const res = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+        headers: {
+          Cookie: `session_id=${session.token}`,
+        },
+      });
+      expect(res.status).toBe(403);
+
+      const responseBody = await res.json();
+
+      expect(responseBody).toEqual({
+        action: 'Check if your user has the feature "create:migration"',
+        message: "You don't have permission to execute this action",
+        name: "ForbiddenError",
+        status_code: 403,
+      });
+    });
+  });
+
+  describe("Privileged user", () => {
+    test("Running pending migrations", async () => {
+      const createdUser = await orchestrator.createUser();
+      const activatedUser = await orchestrator.activateUser(createdUser);
+      await orchestrator.addFeaturesToUser(activatedUser, ["create:migration"]);
+      const session = await orchestrator.createSession(activatedUser.id);
+
+      const res = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+        headers: {
+          Cookie: `session_id=${session.token}`,
+        },
+      });
+      expect(res.status).toBe(200);
+
+      const responseBody = await res.json();
+
+      expect(Array.isArray(responseBody)).toBe(true);
     });
   });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,12 +2,45 @@ import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
 });
 
 describe("GET /api/status", () => {
   describe("Anonymous user", () => {
     test("Retrieving current system status", async () => {
       const res = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await res.json();
+
+      expect(res.status).toBe(200);
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.dependencies.database.max_connections).toBeDefined();
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeDefined();
+
+      const parsedDate = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedDate);
+
+      expect(
+        responseBody.dependencies.database.max_connections,
+      ).toBeGreaterThan(0);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
+
+  describe("Privileged user", () => {
+    test("With 'read:status:all'", async () => {
+      const createdUser = await orchestrator.createUser();
+      const activatedUser = await orchestrator.activateUser(createdUser);
+      await orchestrator.addFeaturesToUser(activatedUser, ["read:status:all"]);
+      const session = await orchestrator.createSession(activatedUser.id);
+
+      const res = await fetch("http://localhost:3000/api/v1/status", {
+        headers: {
+          Cookie: `session_id=${session.token}`,
+        },
+      });
       const responseBody = await res.json();
 
       expect(res.status).toBe(200);
