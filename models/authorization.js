0a084ead0f0f8f9ae533434a82935490209550d6
Gabriel <g_machado1@hotmail.com>
2026-02-05 14:58:55 -0300

    feat: validate 'user', 'feature', and 'resource' in 'authorization' model

--- models/authorization.js
+++ models/authorization.js
@@ -1,4 +1,33 @@
+import { InternalServerError } from "infra/errors";
+
+const AVAILABLE_FEATURES = [
+  // USER
+  "create:user",
+  "read:user",
+  "read:user:self",
+  "update:user",
+  "update:user:others",
+
+  // SESSION
+  "create:session",
+  "read:session",
+
+  // ACTIVATION_TOKEN
+  "read:activation_token",
+
+  // MIGRATION
+  "create:migration",
+  "read:migration",
+
+  // STATUS
+  "read:status",
+  "read:status:all",
+];
+
 function can(user, feature, resource) {
+  validateUser(user);
+  validateFeature(feature);
+
   let authorized = false;
 
   if (user.features.includes(feature)) {
@@ -17,6 +46,10 @@ function can(user, feature, resource) {
 }
 
 function filterOutput(user, feature, resource) {
+  validateUser(user);
+  validateFeature(feature);
+  validateResource(resource);
+
   if (feature === "read:user") {
     return {
       id: resource.id,
@@ -90,6 +123,30 @@ function filterOutput(user, feature, resource) {
   }
 
   return null;
+}
+
+function validateUser(user) {
+  if (!user || !user.features) {
+    throw new InternalServerError({
+      cause: "user is required for authorization model",
+    });
+  }
+}
+
+function validateFeature(feature) {
+  if (!feature || !AVAILABLE_FEATURES.includes(feature)) {
+    throw new InternalServerError({
+      cause: `feature "${feature}" is not known for authorization model`,
+    });
+  }
+}
+
+function validateResource(resource) {
+  if (!resource) {
+    throw new InternalServerError({
+      cause: "resource is required for authorization model",
+    });
+  }
 }
 
 const authorization = {
